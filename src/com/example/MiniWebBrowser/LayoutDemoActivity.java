c50fbc10135a8a1de228ff5c7e4401e14ceec0e3
dimkas71@ukr.net <dimkas71@github.com>
2013-10-28 23:19:29 +0200

    add layout_demo for testing

---
+++ src/com/example/MiniWebBrowser/LayoutDemoActivity.java
@@ -0,0 +1,17 @@
+package com.example.MiniWebBrowser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 28.10.13
+ * Time: 21:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class LayoutDemoActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
